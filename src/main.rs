50340296894a6e0172d1f03014c3bdad08e4cd27
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2025-11-08 00:21:03 +0530

    feat: add check for content length and response status message

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,9 @@ struct Cli {
     /// Target directory
     #[arg(short, long, default_value = ".download")]
     target_directory: String,
+
+    #[arg(short, long, default_value_t = 64)]
+    chunk_size: u16,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -29,15 +32,27 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let response = reqwest::blocking::get(target_file)?;
 
+    if !response.status().is_success() {
+        return Err("Error getting the file.".into());
+    }
+
+    let content_length = response.content_length();
+
+    match content_length {
+        Some(len) => println!("Downloading {} bytes", len),
+        None => println!("Downloading size unknown"),
+    };
+
     let fname = response
         .url()
         .path_segments()
         .and_then(|segments| segments.last())
         .and_then(|name| if name.is_empty() { None } else { Some(name) })
         .unwrap_or("tmp.bin");
-    println!("File to download: '{}'", fname);
+
+    println!("File to download: '{}'.", fname);
     let fname = target_dir.join(fname);
-    println!("File will be located under: '{:?}'", fname);
+    println!("File will be located under: '{}'.", fname.to_str().unwrap());
     let mut dest = File::create(&fname)?;
     let content = response.bytes()?;
     dest.write_all(&content)?;
